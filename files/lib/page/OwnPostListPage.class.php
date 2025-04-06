5676806db3fbc6f4ac6a5f7a64d8b4bb775dfd79
Dennis Kraffczyk <dennis.kraffczyk@gmail.com>
2025-04-06 14:15:52 +0200

    Run PHP CS Fixer to apply PSR-12

--- files/lib/page/OwnPostListPage.class.php
+++ files/lib/page/OwnPostListPage.class.php
@@ -1,31 +1,35 @@
 <?php
+
 namespace wbb\page;
+
 use wcf\system\exception\PermissionDeniedException;
 use wcf\system\WCF;
 
 /**
  * Represents the own post list page.
- * 
- * @author	Dennis Kraffczyk
- * @copyright	2011-2020 KittMedia
- * @license	Free <https://kittmedia.com/licenses/#licenseFree>
- * @package	com.kittmedia.wsf.ownpostlist
- * @category	WoltLabSuite\Forum\Page
+ *
+ * @author  Dennis Kraffczyk
+ * @copyright   2011-2025 KittMedia
+ * @license Free <https://kittmedia.com/licenses/#licenseFree>
+ * @package com.kittmedia.wsf.ownpostlist
+ * @category    WoltLabSuite\Forum\Page
  */
-class OwnPostListPage extends UserPostListPage {
-	/**
-	 * @inheritdoc
-	 */
-	public $loginRequired = true;
-	
-	/**
-	 * @inheritdoc
-	 */
-	public function readParameters() {
-		if (!($this->userID = WCF::getUser()->getObjectID())) {
-			throw new PermissionDeniedException();
-		}
-		
-		parent::readParameters();
-	}
+class OwnPostListPage extends UserPostListPage
+{
+    /**
+     * @inheritdoc
+     */
+    public $loginRequired = true;
+
+    /**
+     * @inheritdoc
+     */
+    public function readParameters()
+    {
+        if (!($this->userID = WCF::getUser()->getObjectID())) {
+            throw new PermissionDeniedException();
+        }
+
+        parent::readParameters();
+    }
 }
--- files/lib/system/page/handler/OwnPostListPageHandler.class.php
+++ files/lib/system/page/handler/OwnPostListPageHandler.class.php
@@ -1,22 +1,26 @@
 <?php
+
 namespace wbb\system\page\handler;
+
 use wcf\system\page\handler\AbstractMenuPageHandler;
 use wcf\system\WCF;
 
 /**
  * Page handler for own posts.
- * 
- * @author	Dennis Kraffczyk
- * @copyright	2011-2020 KittMedia
- * @license	Free <https://kittmedia.com/licenses/#licenseFree>
- * @package	com.kittmedia.wsf.ownpostlist
- * @category	WoltLabSuite\Forum\System\Page\Handler
+ *
+ * @author  Dennis Kraffczyk
+ * @copyright   2011-2025 KittMedia
+ * @license Free <https://kittmedia.com/licenses/#licenseFree>
+ * @package com.kittmedia.wsf.ownpostlist
+ * @category    WoltLabSuite\Forum\System\Page\Handler
  */
-class OwnPostListPageHandler extends AbstractMenuPageHandler {
-	/**
-	 * @inheritdoc
-	 */
-	public function isVisible($objectID = null) {
-		return WCF::getUser()->getObjectID();
-	}
+class OwnPostListPageHandler extends AbstractMenuPageHandler
+{
+    /**
+     * @inheritdoc
+     */
+    public function isVisible($objectID = null)
+    {
+        return WCF::getUser()->getObjectID();
+    }
 }
